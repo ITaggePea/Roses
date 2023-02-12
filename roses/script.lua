b19c569a385fbc873e1f39d3ce7b877c86aedbc2
ITaggePea <116766469+ITaggePea@users.noreply.github.com>
2023-02-12 15:47:44 -0300

    vercion final?

--- roses/script.lua
+++ roses/script.lua
@@ -1,5 +1,5 @@
 function onCreate()
-	makeHealthIcon('iconP4', 'dad', false)
+    makeHealthIcon('iconP4', 'dad', false)
     setObjectOrder('iconP4', getObjectOrder('iconP2') + 1)
     setProperty('iconP4.alpha', 0)
 
@@ -7,99 +7,74 @@ function onCreate()
     setObjectOrder('iconP3', getObjectOrder('iconP1') + 1)
     setProperty('iconP3.alpha', 0)
 
-	iconY = getProperty('iconP1.y')
+    iconY = getProperty('iconP1.y')
 end
 
 iconY = 0
 
-function onUpdate(elapsed) 
-	     local currentBeat = (songPos / 1000)*(bpm/60)
+function update (elapsed)
+    local currentBeat = (songPos / 1000)*(bpm/60)
 
-          setObjectOrder('iconP3', getObjectOrder('iconP1') + 1)
+    if (getProperty('dad.animation.name') == 'idle' or getProperty('dad.animation.name') == 'danceRight') and (getProperty('dad1.animation.name') == 'idle' or getProperty('dad1.animation.name') == 'danceRight') and mustHitSection == false and stopFollow == false then
+        setCamFollow(xx, yy)
+    end
 
-		if getProperty('dad1.alpha') == 1 then
-               setObjectOrder('dad1', getObjectOrder('dad') + 1)
-		end
+    if (getProperty('boyfriend.animation.name') == 'idle' or getProperty('boyfriend.animation.name') == 'danceRight') and mustHitSection == true and stopFollow == false and (getProperty('boyfriend1.animation.name') == 'idle' or getProperty('boyfriend1.animation.name') == 'danceRight') then
+        setCamFollow(xx2, yy2)
+    end
 
-		if getProperty('boyfriend1.alpha') == 1 then
-               setObjectOrder('boyfriend1', getObjectOrder('boyfriend') + 1)
-		end
+    if getProperty('iconP4.alpha') == 1 then
+        setProperty('iconP4.scale.x', getProperty('iconP2.scale.x'))
+        setProperty('iconP4.scale.y', getProperty('iconP2.scale.y'))
+        setProperty('iconP4.animation.curAnim.curFrame', getProperty('iconP2.animation.curAnim.curFrame'))
+    end
 
-		if getProperty('iconP4.alpha') == 1 then
-			setProperty('iconP4.scale.x', getProperty('iconP2.scale.x'))
-			setProperty('iconP4.scale.y', getProperty('iconP2.scale.y'))
-	
-			setProperty('iconP4.animation.curAnim.curFrame', getProperty('iconP2.animation.curAnim.curFrame'))
-		end
-	
-		if getProperty('iconP3.alpha') == 1 then
-			setProperty('iconP3.scale.x', getProperty('iconP1.scale.x'))
-			setProperty('iconP3.scale.y', getProperty('iconP1.scale.y'))
-			setProperty('iconP3.animation.curAnim.curFrame', getProperty('iconP1.animation.curAnim.curFrame'))
-		end
-		
-		if useDoubleDadIcons == true then
-			setProperty('opponentIconScale', 1.05)
-			setProperty('iconP4.x', getProperty('iconP2.x') - 50)
-			setProperty('iconP4.alpha', 1)
-		end
-	
-		if useDoubleBFIcons == true then
-			setProperty('playerIconScale', 1.05)
-			setProperty('iconP3.x', getProperty('iconP1.x') + 50)
-			setProperty('iconP3.y', getProperty('iconP1.y') + 40)
-			setProperty('iconP3.x', getProperty('iconP1.x') + getProperty('newpoint.x'))
-			setProperty('iconP3.alpha', 1)
-		end
+    if getProperty('iconP3.alpha') == 1 then
+        setProperty('iconP3.scale.x', getProperty('iconP1.scale.x'))
+        setProperty('iconP3.scale.y', getProperty('iconP1.scale.y'))
+        setProperty('iconP3.animation.curAnim.curFrame', getProperty('iconP1.animation.curAnim.curFrame'))
+    end
 
-		if useDoubleDadIcons == false and getProperty('opponentIconScale') ~= 1.2 then
-			setProperty('opponentIconScale', 1.2)
-		end
+    if useDoubleDadIcons then
+        setProperty('opponentIconScale', 1.05)
+        setProperty('iconP4.x', getProperty('iconP2.x') - 50)
+        setProperty('iconP4.y', getProperty('iconP2.y') + 30)
+        setProperty('iconP4.alpha', 1)
+    end
 
-		if useDoubleBFIcons == false and getProperty('playerIconScale') ~= 1.2 then
-			setProperty('playerIconScale', 1.2)
-		end
-	end
+    if useDoubleBFIcons then
+        setProperty('playerIconScale', 1.05)
+        setProperty('iconP3.x', getProperty('iconP1.x') + 40)
+        setProperty('iconP3.y', getProperty('iconP1.y') + 30)
+        setProperty('iconP3.alpha', 1)
+    end
 
-function onUpdatePost()
-	if useDoubleBFIcons == true then
-		if getProperty('iconP1.y') ~= iconY - 20 then
-			setProperty('iconP1.y', iconY - 20)
-			setProperty('iconP3.y', getProperty('iconP1.y') + 40)
-		end
+    if getProperty('dad1.alpha') == 1 then
+        setObjectOrder('dad1', getObjectOrder('dad') - 1)
+    end
 
-		if getProperty('iconP3.animation.name') == 'solid' and curStep >= 1056 and curStep < 1152 then
-			setProperty('iconP3.scale.x', getProperty('iconP1.scale.x') + 0.15)
-			setProperty('iconP3.scale.y', getProperty('iconP1.scale.y') + 0.15)
-		end
-	end
-
-	if useDoubleDadIcons == true then
-		if getProperty('iconP2.y') ~= iconY - 20 then
-			setProperty('iconP2.y', iconY - 20)
-			setProperty('iconP4.y', getProperty('iconP2.y') + 40)
-		end
-	end
+    if getProperty('boyfriend1.alpha') == 1 then
+        setObjectOrder('boyfriend1', getObjectOrder('boyfriend') - 1)
+    end
 end
 
 useDoubleDadIcons = false
 useDoubleBFIcons = false
 
-
 function onStepHit(step)
      if curStep == 64 then
          cameraFlash('game', '0xFFFFFFFF', 0.7)
          changeDadAuto('2vplus-senpai-angry')
-         changeStage('senpaihd')
          changeBFAuto('bf')
          changeGFAuto('gf')
-         setupNoteSplash('-normal')
-         changeStaticNotes('normal', 'normal')
+
+	    changeNotesDad('normal')
+	    changeNotesBF('normal')
      end
 
      if curStep == 132 then
-        changeDadAuto('botan')
-        setupNoteSplash('-holofunk')
+         changeDadAuto('botan')
+	    changeNotesDad('holofunk')
      end
 
      if curStep == 164 then
@@ -108,6 +83,7 @@ function onStepHit(step)
     
      if curStep == 192 then
          changeDadAuto('shaggy')
+	    changeNotesDad('normal')
      end
 
      if curStep == 224 then
@@ -140,11 +116,11 @@ function onStepHit(step)
 
      if curStep == 448 then
          changeDadAuto('majin-new')
-         setupNoteSplash('-majin')
+	    changeNotesDad('majin')
      end
 
      if curStep == 480 then
-         changeBFAuto('sunky')
+         changeBFAuto('sunky-new')
      end
   
 	if curStep == 515 then
@@ -152,57 +128,51 @@ function onStepHit(step)
          useDoubleBFIcons = true
 
          cameraFlash('game', '0xFFFFFFFF', 0.7, true)
+	    changeNotesDad('normal')
 
          makeLuaCharacter('dad1', 'dad', false, false)
          changeDadAuto('mom')
          setProperty('iconP4.alpha', 1)
+	    setProperty('dad.x', getProperty('dad.x') - 225)
 
          makeLuaCharacter('boyfriend1', 'bf-gfV2', true, false)
          changeBFAuto('bf')
+         changeGFAuto('nogf')
          setProperty('iconP3.alpha', 1)
-    end
+	    setProperty('boyfriend.x', getProperty('boyfriend.x') + 225)
+	    setProperty('boyfriend1.y', getProperty('boyfriend1.y') - 25)
+     end
 
 	if curStep == 576 then 
          changeLuaCharacter('dad1', 'ruv') 
          changeDadAuto('sarvente')
 	    changeAddedIcon('iconP4', getProperty('dad1.healthIcon'))
-    end
+	    setProperty('dad.x', getProperty('dad.x') - 225)
+     end
 
 	if curStep == 608 then
-        makeLuaCharacter('boyfriend1', 'carol', true, false)
-	   setProperty('boyfriend1.x', getProperty('boyfriend1.x') + 200)
-        setObjectOrder('boyfriend1', getObjectOrder('boyfriend') - 1)
-
-        makeHealthIcon('bfIcon1', 'carol', true)
-        setObjectOrder('bfIcon1', getObjectOrder('iconP1') + 1)
-        setProperty('bfIcon1.alpha', 0)
-  
-        setProperty('boyfriend1.alpha', 0)
-    end
+         changeLuaCharacter('boyfriend1', 'carol') 
+         changeBFAuto('whitty')
+         changeGFAuto('gf')
+	    changeAddedIcon('iconP3', getProperty('boyfriend1.healthIcon'))
+	    setProperty('boyfriend.x', getProperty('boyfriend.x') + 225)
+     end
 
 	if curStep == 640 then
-        makeLuaCharacter('dad1', 'agoti', false, false)
-        setProperty('dad1.x', getProperty('dad1.x') + 200)
-        setObjectOrder('dad1', getObjectOrder('dad') - 1)
-
-        makeHealthIcon('dadIcon1', 'agoti', false)
-        setObjectOrder('dadIcon1', getObjectOrder('iconP2') + 1)
-        setProperty('dadIcon1.alpha',2)
-
-        setProperty('dad1.alpha', 1)
-    end
+         changeLuaCharacter('dad1', 'agoti') 
+         changeDadAuto('tabi')
+	    changeAddedIcon('iconP4', getProperty('dad1.healthIcon'))
+	    setProperty('dad.x', getProperty('dad.x') - 225)
+	    changeNotesDad('agoti')
+     end
 
 	if curStep == 672 then
-        makeLuaCharacter('boyfriend1', 'ruby', true, false)
-	   setProperty('boyfriend1.x', getProperty('boyfriend1.x') + 200)
-        setObjectOrder('boyfriend1', getObjectOrder('boyfriend') - 1)
-
-        makeHealthIcon('bfIcon1', 'ruby', true)
-        setObjectOrder('bfIcon1', getObjectOrder('iconP1') + 1)
-        setProperty('bfIcon1.alpha', 0)
-  
-        setProperty('boyfriend1.alpha', 0)
-    end
+         changeLuaCharacter('boyfriend1', 'ruby') 
+         changeBFAuto('cj')
+	    changeAddedIcon('iconP3', getProperty('boyfriend1.healthIcon'))
+	    setProperty('boyfriend.x', getProperty('boyfriend.x') + 225)
+	    changeNotesBF('Cj-Ruby')
+     end
 end
 
 function playerOneTurn()
@@ -266,16 +236,38 @@ function bfNoteHit(note, isSustain, noteType, dType)
 
 end
 
-function playerOneMiss(note, songPos, dType)
-
-end
-
-function onTweenCompleted(t, tag)
-	if tag == 'colorTween' then
-		tweenColor('white', 0.2, RGBColor(255,216,0), RGBColor(255,255,255))
+function changeNotesDad(skin)
+	for i = 0,3 do
+		setPropertyFromGroup('strumLineNotes', i, 'texture', skin)
 	end
 
-	if tag == 'reverseColorTween' then
-		tweenColor('white', 0.2, RGBColor(255,216,0), RGBColor(255,0,0))
+	local daSection = curSection
+
+	if daSection % 2 == 1 then -- in case the event moves back 1
+		daSection = curSection + 1
+	end
+
+	for i = 0,70 do
+		if getPropertyFromGroup('notes', i, 'noteSection') == daSection then
+			setPropertyFromGroup('notes', i, 'texture', skin)
+		end
+	end
+end
+
+function changeNotesBF(skin)
+	for i = 4,7 do
+		setPropertyFromGroup('strumLineNotes', i, 'texture', skin)
+	end
+
+	local daSection = curSection
+
+	if daSection % 2 == 1 then -- in case the event moves back 1
+		daSection = curSection + 1
+	end
+
+	for i = 0,70 do
+		if getPropertyFromGroup('notes', i, 'noteSection') == daSection then
+			setPropertyFromGroup('notes', i, 'texture', skin)
+		end
 	end
 end
